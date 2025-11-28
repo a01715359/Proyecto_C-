039531289814822ee2471f8777434e8ec402654a
a01715359 <a01715359@tec.mx>
2025-11-28 10:51:26 -0600

    Remove const qualifier from getter methods

--- Sim.h
+++ Sim.h
@@ -15,36 +15,36 @@ class Sim{
         Sim(): numero(""), operador(""), tipo(""), activo(false){};
         Sim(std::string num_sim, std::string oper_sim, std::string tip_sim, bool act_sim): numero(num_sim), operador(oper_sim), tipo(tip_sim), activo(act_sim){}
 
-        std::string getNumero() const;
-        std::string getOperador() const;
-        std::string getTipo() const;
-        bool getActivo() const;
+    std::string getNumero();
+    std::string getOperador();
+    std::string getTipo();
+    bool getActivo();
 
-        void setActivo(bool act_sim);
-        void mostrar_info() const;
+    void setActivo(bool act_sim);
+    void mostrar_info();
 };
 
-std::string Sim::getNumero() const {
+std::string Sim::getNumero(){
     return numero;
 }
 
-std::string Sim::getOperador() const {
+std::string Sim::getOperador(){
     return operador;
 }
 
-std::string Sim::getTipo() const {
+std::string Sim::getTipo(){
     return tipo;
 }
 
-bool Sim::getActivo() const {
+bool Sim::getActivo(){
     return activo;
 }
 
-void Sim::setActivo(bool act_sim) {
+void Sim::setActivo(bool act_sim){
     activo = act_sim;
 }
 
-void Sim::mostrar_info() const {
+void Sim::mostrar_info(){
     std::cout << "Número: " << numero << std::endl;
     std::cout << "Operador: " << operador << std::endl;
     std::cout << "Tipo: " << tipo << std::endl;
