f243d899ffe5752844f644631b6dff5dd262d868
a01715359 <a01715359@tec.mx>
2025-12-02 11:45:44 -0600

    Update Laptop.h

--- Laptop.h
+++ Laptop.h
@@ -1,56 +1,47 @@
-#ifndef SMARTPHONE_H
-#define SMARTPHONE_H
+#ifndef LAPTOP_H
+#define LAPTOP_H
 
 #include <iostream>
-#include <string>
+#include <string>   
 #include "Electronico.h"
-#include "Sim.h"
 #include <sstream>
 #include <limits>
 
-class Smartphone: public Electronico{
+class Laptop: public Electronico{
     private:
-        int memoria;
-        Sim sim;
+        std::string procesador;
+        int ram;
 
     public:
-    Smartphone(): Electronico(), memoria(0), sim() {};
-    Smartphone(std::string nom, float pre, std::string mar, int mem, float tam, std::string cond, int cant, std::string num_sim, std::string oper_sim, std::string tip_sim,  bool act_sim)
-    : Electronico(nom, pre, mar, tam, cond, cant), memoria(mem), sim(num_sim, oper_sim, tip_sim, act_sim) {}
+        Laptop(): Electronico(), procesador(""), ram(0){};
+        Laptop(std::string nom, float pre, std::string mar, std::string pro, float tam, int r, std::string cond, int cant): Electronico(nom, pre, mar, tam, cond, cant), procesador(pro), ram(r){}
 
-    int getMemoria();
-    Sim getSim();
-    void setSim(Sim &s);
+    std::string getProcesador();
+    int getRam();
 
     std::string mostrar_info();
+
 };
 
-        int Smartphone::getMemoria() {
-            return memoria;
+        std::string Laptop::getProcesador(){
+            return procesador;
         }
 
-        Sim Smartphone::getSim() {
-            return sim;
+        int Laptop::getRam(){
+            return ram;
         }
 
-        void Smartphone::setSim(Sim &s) {
-            sim = s;
-        }
-
-        std::string Smartphone::mostrar_info() {
+        std::string Laptop::mostrar_info(){
             std::stringstream info;
             info << "Nombre: " << nombre << std::endl;
             info << "Precio: $" << precio << std::endl;
             info << "Marca: " << marca << std::endl;
-            info << "Memoria: " << memoria << " GB" << std::endl;
-            info << "SIM: \n" << sim.mostrar_info();
-            info << " - " << sim.getNumero() << " (" << sim.getOperador() << ")" << std::endl;
-            info << " - " << sim.getTipo() << " (" << (sim.getActivo() ? "Activo" : "Inactivo") << ")" << std::endl;
-            info << "Condición: " << condicion << std::endl;
+            info << "Procesador: " << procesador << std::endl;
             info << "Tamaño: " << tamano << " pulgadas" << std::endl;
+            info << "RAM: " << ram << " GB" << std::endl;
+            info << "Condición: " << condicion << std::endl;
             info << "Cantidad disponible: " << cantidad_disponible << std::endl;
             info << "Valor total en stock: $" << calcular_valor_stock() << std::endl;
                 return info.str();
         }
-
 #endif
