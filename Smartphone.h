a9d11854251594643d414ef007c4f798fb638849
a01715359 <a01715359@tec.mx>
2025-12-04 10:50:43 -0600

    Update Smartphone.h

--- Smartphone.h
+++ Smartphone.h
@@ -6,7 +6,6 @@
 #include "Electronico.h"
 #include "Sim.h"
 #include <sstream>
-#include <limits>
 
 class Smartphone: public Electronico{
     private:
@@ -14,43 +13,42 @@ class Smartphone: public Electronico{
         Sim sim;
 
     public:
-    Smartphone(): Electronico(), memoria(0), sim() {};
-    Smartphone(std::string nom, float pre, std::string mar, int mem, float tam, std::string cond, int cant, std::string num_sim, std::string oper_sim, std::string tip_sim,  bool act_sim)
-    : Electronico(nom, pre, mar, tam, cond, cant), memoria(mem), sim(num_sim, oper_sim, tip_sim, act_sim) {}
+    Smartphone(): 
+        Electronico(), memoria(0), sim() {};
+    Smartphone(std::string nom, float pre, std::string mar, int mem, float tam, std::string cond, int cant, 
+                std::string numSim, std::string operSim, std::string tipSim,  bool actSim):
+        Electronico(nom, pre, mar, tam, cond, cant), memoria(mem), sim(numSim, operSim, tipSim, actSim) {}
 
     int getMemoria();
     Sim getSim();
     void setSim(Sim &s);
 
-    std::string mostrar_info();
+    std::string mostrarInfo();
 };
 
-        int Smartphone::getMemoria() {
-            return memoria;
-        }
+int Smartphone::getMemoria() {
+    return memoria;
+}
 
-        Sim Smartphone::getSim() {
-            return sim;
-        }
+Sim Smartphone::getSim() {
+    return sim;
+}
 
-        void Smartphone::setSim(Sim &s) {
-            sim = s;
-        }
+void Smartphone::setSim(Sim &s) {
+    sim = s;
+}
 
-        std::string Smartphone::mostrar_info() {
-            std::stringstream info;
-            info << "Nombre: " << nombre << std::endl;
-            info << "Precio: $" << precio << std::endl;
-            info << "Marca: " << marca << std::endl;
-            info << "Memoria: " << memoria << " GB" << std::endl;
-            info << "SIM: \n" << sim.mostrar_info();
-            info << " - " << sim.getNumero() << " (" << sim.getOperador() << ")" << std::endl;
-            info << " - " << sim.getTipo() << " (" << (sim.getActivo() ? "Activo" : "Inactivo") << ")" << std::endl;
-            info << "Condición: " << condicion << std::endl;
-            info << "Tamaño: " << tamano << " pulgadas" << std::endl;
-            info << "Cantidad disponible: " << cantidad_disponible << std::endl;
-            info << "Valor total en stock: $" << calcular_valor_stock() << std::endl;
-                return info.str();
-        }
-
+std::string Smartphone::mostrarInfo() {
+    std::stringstream info;
+    info << "Nombre: " << nombre << std::endl;
+    info << "Precio: $" << precio << std::endl;
+    info << "Marca: " << marca << std::endl;
+    info << "Memoria: " << memoria << " GB" << std::endl;
+    info << "SIM: \n" << sim.mostrarInfo();
+    info << "Condicion: " << condicion << std::endl;
+    info << "Size: " << tamano << " pulgadas" << std::endl;
+    info << "Cantidad disponible: " << cantidadDisponible << std::endl;
+    info << "Valor total en stock: $" << calcularValorStock() << std::endl;
+    return info.str();
+}
 #endif
