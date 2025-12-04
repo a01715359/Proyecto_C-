5216486f2152d4a255e320a1172f3f2fa19f0a9e
a01715359 <a01715359@tec.mx>
2025-12-04 10:27:45 -0600

    Update Inventario.h

--- Inventario.h
+++ Inventario.h
@@ -14,107 +14,106 @@ class Inventario{
     Laptop laptops[1000];
     Smartphone smartphones[1000];
     Televisor televisores[1000];
-    int cantidad_laptops;
-    int cantidad_smartphones;
-    int cantidad_televisores;
+    int cantidadLaptops;
+    int cantidadSmartphones;
+    int cantidadTelevisores;
 
     public:
-    Inventario(): cantidad_laptops(0), cantidad_smartphones(0), cantidad_televisores(0){}
+    Inventario(): cantidadLaptops(0), cantidadSmartphones(0), cantidadTelevisores(0){}
 
-    int getCantidad_laptops(){
-        return cantidad_laptops;
-    }
-    int getCantidad_smartphones(){
-        return cantidad_smartphones;
-    }
-    int getCantidad_televisores(){
-        return cantidad_televisores;
-    }
+    int getCantidadLaptops();
+    int getCantidadSmartphones();
+    int getCantidadTelevisores();
 
     void agregarLaptop(Laptop& laptop);
     void agregarSmartphone(Smartphone& smartphone);
     void agregarTelevisor(Televisor& televisor);
-    void modificarLaptop(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad);
-    void modificarSmartphone(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad);
-    void modificarTelevisor(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad);
+    void modificarLaptop(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad);
+    void modificarSmartphone(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad);
+    void modificarTelevisor(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad);
     std::string mostrarInventario();
 };
 
-    void Inventario:: agregarLaptop(Laptop& laptop){
-        if (cantidad_laptops < 1000) {
-            laptops[cantidad_laptops] = laptop;
-            cantidad_laptops++;
+int Inventario::getCantidadLaptops(){
+        return cantidadLaptops;
+    }
+    int Inventario::getCantidadSmartphones(){
+        return cantidadSmartphones;
+    }
+    int Inventario::getCantidadTelevisores(){
+        return cantidadTelevisores;
+    }
+
+    void Inventario::agregarLaptop(Laptop& laptop){
+        if (cantidadLaptops < 1000) {
+            laptops[cantidadLaptops] = laptop;
+            cantidadLaptops++;
         } else {
             std::cout << "No se puede agregar más laptops: inventario lleno.\n";
         }
     }
 
     void Inventario::agregarSmartphone(Smartphone& smartphone){
-        if (cantidad_smartphones < 1000) {
-            smartphones[cantidad_smartphones] = smartphone;
-            cantidad_smartphones++;
+        if (cantidadSmartphones < 1000) {
+            smartphones[cantidadSmartphones] = smartphone;
+            cantidadSmartphones++;
         } else {
             std::cout << "No se puede agregar más smartphones: inventario lleno.\n";
         }
     }
 
     void Inventario::agregarTelevisor(Televisor& televisor){
-        if (cantidad_televisores < 1000) {
-            televisores[cantidad_televisores] = televisor;
-            cantidad_televisores++;
+        if (cantidadTelevisores < 1000) {
+            televisores[cantidadTelevisores] = televisor;
+            cantidadTelevisores++;
         } else {
             std::cout << "No se puede agregar más televisores: inventario lleno.\n";
         }
     }
 
-    
-
-    void Inventario::modificarLaptop(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad){
-        if (index >= 0 && index < cantidad_laptops){
-            laptops[index].setPrecio(nuevo_precio);
-            laptops[index].setCondicion(nueva_condicion);
-            laptops[index].setCantidad_disponible(nueva_cantidad);
+    void Inventario::modificarLaptop(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad){
+        if (index >= 0 && index < cantidadLaptops){
+            laptops[index].setPrecio(nuevoPrecio);
+            laptops[index].setCondicion(nuevaCondicion);
+            laptops[index].setCantidadDisponible(nuevaCantidad);
         } else {
-            std::cout << "Índice inválido para laptop.\n";
+            std::cout << "Indice invalido para laptop.\n";
         }
     }
 
-    void Inventario::modificarSmartphone(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad){
-        if (index >= 0 && index < cantidad_smartphones){
-            smartphones[index].setPrecio(nuevo_precio);
-            smartphones[index].setCondicion(nueva_condicion);
-            smartphones[index].setCantidad_disponible(nueva_cantidad);
+    void Inventario::modificarSmartphone(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad){
+        if (index >= 0 && index < cantidadSmartphones){
+            smartphones[index].setPrecio(nuevoPrecio);
+            smartphones[index].setCondicion(nuevaCondicion);
+            smartphones[index].setCantidadDisponible(nuevaCantidad);
         } else {
             std::cout << "Índice inválido para smartphone.\n";
         }
     }
 
-    void Inventario::modificarTelevisor(int index, float nuevo_precio, std::string nueva_condicion, int nueva_cantidad){
-        if (index >= 0 && index < cantidad_televisores){
-            televisores[index].setPrecio(nuevo_precio);
-            televisores[index].setCondicion(nueva_condicion);
-            televisores[index].setCantidad_disponible(nueva_cantidad);
+    void Inventario::modificarTelevisor(int index, float nuevoPrecio, std::string nuevaCondicion, int nuevaCantidad){
+        if (index >= 0 && index < cantidadTelevisores){
+            televisores[index].setPrecio(nuevoPrecio);
+            televisores[index].setCondicion(nuevaCondicion);
+            televisores[index].setCantidadDisponible(nuevaCantidad);
         } else {
-            std::cout << "Índice inválido para televisor.\n";
+            std::cout << "Indice invalido para televisor.\n";
         }
     }
 
     std::string Inventario::mostrarInventario(){
         std::stringstream ss;
         ss << "=== Laptops ===\n";
-        for(int i = 0; i < cantidad_laptops; i++){
-            ss << "[" << i << "]\n";
-            ss << laptops[i].mostrar_info();
+        for(int i = 0; i < cantidadLaptops; i++){
+            ss << laptops[i].mostrarInfo();
         }
         ss << "=== Smartphones ===\n";
-        for(int i = 0; i < cantidad_smartphones; i++){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-            ss << "[" << i << "]\n";
-            ss << smartphones[i].mostrar_info();
+        for(int i = 0; i < cantidadSmartphones; i++){
+            ss << smartphones[i].mostrarInfo();
         }
         ss << "=== Televisores ===\n";
-        for(int i = 0; i < cantidad_televisores; i++){
-            ss << "[" << i << "]\n";
-            ss << televisores[i].mostrar_info();
+        for(int i = 0; i < cantidadTelevisores; i++){
+            ss << televisores[i].mostrarInfo();
         }
         return ss.str();
     }
