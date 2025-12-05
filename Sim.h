c799ffd6ec1be7baa2d15d547090052cb74cdb03
a01715359 <a01715359@tec.mx>
2025-12-04 19:18:45 -0600

    Update Sim.h

--- Sim.h
+++ Sim.h
@@ -2,21 +2,21 @@
 #define SIM_H
 
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
-class Sim{
-    private:
-        std::string numero;
-        std::string operador;
-        std::string tipo;
-        bool activo;
+class Sim {
+   private:
+    std::string numero;
+    std::string operador;
+    std::string tipo;
+    bool activo;
 
-    public:
-        Sim(): 
-            numero(""), operador(""), tipo(""), activo(false){};
-        Sim(std::string numSim, std::string operSim, std::string tipSim, bool actSim): 
-            numero(numSim), operador(operSim), tipo(tipSim), activo(actSim){}
+   public:
+    Sim() : numero(""), operador(""), tipo(""), activo(false) {};
+    Sim(std::string numSim, std::string operSim, std::string tipSim,
+        bool actSim)
+        : numero(numSim), operador(operSim), tipo(tipSim), activo(actSim) {}
 
     std::string getNumero();
     std::string getOperador();
@@ -27,27 +27,27 @@ class Sim{
     std::string mostrarInfo();
 };
 
-std::string Sim::getNumero(){
+std::string Sim::getNumero() {
     return numero;
 }
 
-std::string Sim::getOperador(){
+std::string Sim::getOperador() {
     return operador;
 }
 
-std::string Sim::getTipo(){
+std::string Sim::getTipo() {
     return tipo;
 }
 
-bool Sim::getActivo(){
+bool Sim::getActivo() {
     return activo;
 }
 
-void Sim::setActivo(bool actSim){
+void Sim::setActivo(bool actSim) {
     activo = actSim;
 }
 
-std::string Sim::mostrarInfo(){
+std::string Sim::mostrarInfo() {
     std::stringstream info;
     info << "Número: " << numero << std::endl;
     info << "Operador: " << operador << std::endl;
@@ -55,5 +55,4 @@ std::string Sim::mostrarInfo(){
     info << "Estado: " << (activo ? "Activo" : "Inactivo") << std::endl;
     return info.str();
 }
-
 #endif
