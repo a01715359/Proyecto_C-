b899f3a425b8b3625e12a5efd4265e455740a83f
a01715359 <a01715359@tec.mx>
2025-12-04 19:15:26 -0600

    Update Laptop.h

--- Laptop.h
+++ Laptop.h
@@ -2,47 +2,49 @@
 #define LAPTOP_H
 
 #include <iostream>
-#include <string>   
-#include "Electronico.h"
 #include <sstream>
+#include <string>
 
-class Laptop: public Electronico{
-    private:
-        std::string procesador;
-        int ram;
+#include "Electronico.h"
 
-    public:
-        Laptop(): 
-            Electronico(), procesador(""), ram(0){};
-        Laptop(std::string nom, float pre, std::string mar, std::string pro, float tam, int r, std::string cond, int cant): 
-            Electronico(nom, pre, mar, tam, cond, cant), procesador(pro), ram(r){}
+class Laptop : public Electronico {
+   private:
+    std::string procesador;
+    int ram;
+
+   public:
+    Laptop() : Electronico(), procesador(""), ram(0) {};
+    Laptop(std::string nom, float pre, std::string mar, std::string pro,
+           float tam, int r, std::string cond, int cant)
+        : Electronico(nom, pre, mar, tam, cond, cant),
+          procesador(pro),
+          ram(r) {}
 
     std::string getProcesador();
     int getRam();
 
     std::string mostrarInfo();
-
 };
 
-std::string Laptop::getProcesador(){
-return procesador;
+std::string Laptop::getProcesador() {
+    return procesador;
 }
 
-int Laptop::getRam(){
-return ram;
+int Laptop::getRam() {
+    return ram;
 }
 
-std::string Laptop::mostrarInfo(){
-std::stringstream info;
-info << "Nombre: " << nombre << std::endl;
-info << "Precio: $" << precio << std::endl;
-info << "Marca: " << marca << std::endl;
-info << "Procesador: " << procesador << std::endl;
-info << "Size: " << tamano << " pulgadas" << std::endl;
-info << "RAM: " << ram << " GB" << std::endl;
-info << "Condicion: " << condicion << std::endl;
-info << "Cantidad disponible: " << cantidadDisponible << std::endl;
-info << "Valor total en stock: $" << calcularValorStock() << std::endl;
+std::string Laptop::mostrarInfo() {
+    std::stringstream info;
+    info << "Nombre: " << nombre << std::endl;
+    info << "Precio: $" << precio << std::endl;
+    info << "Marca: " << marca << std::endl;
+    info << "Procesador: " << procesador << std::endl;
+    info << "Size: " << tamano << " pulgadas" << std::endl;
+    info << "RAM: " << ram << " GB" << std::endl;
+    info << "Condicion: " << condicion << std::endl;
+    info << "Cantidad disponible: " << cantidadDisponible << std::endl;
+    info << "Valor total en stock: $" << calcularValorStock() << std::endl;
     return info.str();
 }
 #endif
